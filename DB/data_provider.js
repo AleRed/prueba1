52a699c9e8176fa7273a01ada03106d57d30540e
AleRed <ale.redondo92@gmail.com>
2016-05-02 17:29:27 +0200

    Correcciones en las funciones que añaden graficas con filtros a la interfaz para line_chart. bar_chart y pie_chart

--- DB/data_provider.js
+++ DB/data_provider.js
@@ -90,6 +90,136 @@ data_provider.getXYData = function(graphic_type, group_by){
   }
 };
 
+
+/**
+ *
+ *
+ *
+ * */
+data_provider.getTreemapData = function(group_by) {
+  if (group_by == "department"){
+    var table1 = "departments";
+    var table2 = "provinces";
+    var table3 = "payment_types";
+
+  } else if (group_by == "store"){
+    var table1 = "provinces";
+    var table2 = "payment_types";
+    var table3 = "departments";
+
+  } else if (group_by == "payment_type"){
+    var table1 = "payment_types";
+    var table2 = "departments";
+    var table3 = "provinces";
+  }
+
+  return db.Sale.query(function(q) {
+    q.column([table1 + ".name as level1", table2 + ".name as level2", table3 + ".name as key"]).sum('value as value')
+      .groupBy(table1 + '.name')
+      .groupBy(table2 + '.name')
+      .groupBy(table3 + '.name')
+      .innerJoin('departments', 'sales.department_id', 'departments.id')
+      .innerJoin('stores', 'sales.store_id', 'stores.id')
+      .innerJoin('provinces', 'stores.province_id', 'provinces.id')
+      .innerJoin('payment_types', 'sales.payment_type_id', 'payment_types.id')
+  }).fetchAll()
+    .then(function(data){
+      return new Promise(function(resolve){
+        return resolve(data.toJSON());
+      })
+    });
+};
+
+
+/**
+ *
+ *
+ *
+ * */
+data_provider.getXYDataWithFilter = function(graphic_type, group_by, filter_type, filter_value){
+  if(graphic_type == "line_chart" || graphic_type == "bar_chart"){
+    var x_name = "x";
+    var y_name = "y";
+  } else if(graphic_type == "pie_chart") {
+    var x_name = "key";
+    var y_name = "value";
+  }
+  var filter_table_name = filter_type + "s";
+
+  if(group_by == "department" || group_by == "payment_type" || group_by == "store"){
+    var table_name = group_by + "s";
+    var id_name = group_by + "_id";
+    return db.Sale.query(function(q){
+
+      q.column(table_name + '.name as ' + x_name).sum('value as ' + y_name)
+        .whereRaw(filter_table_name + '.name = "' + filter_value + '"')
+        .groupBy(id_name)
+        .innerJoin('departments', 'sales.department_id', 'departments.id')
+        .innerJoin('payment_types', 'sales.payment_type_id', 'payment_types.id')
+        .innerJoin('stores', 'sales.store_id', 'stores.id');
+
+    }).fetchAll()
+      .then(function(data){
+        return new Promise(function(resolve){
+          return resolve(data.toJSON());
+        })
+      });
+
+  } else if(group_by == "date_month"){
+    return db.Sale.query(function(q){
+      q.column("sales.date as x").sum('value as y')
+        .whereRaw(filter_table_name + '.name = "' + filter_value + '"')
+        .groupByRaw("YEAR(sales.date)*100+MONTH(sales.date)")
+        .orderByRaw("YEAR(sales.date), MONTH(sales.date)")
+        .innerJoin('departments', 'sales.department_id', 'departments.id')
+        .innerJoin('payment_types', 'sales.payment_type_id', 'payment_types.id');
+
+    }).fetchAll()
+      .then(function(data){
+        return new Promise(function(resolve){
+          var json_data = data.toJSON();
+          var new_data = [];
+          json_data.forEach(function(d){
+            var date = moment(d.x);
+            var new_element = {};
+            new_element[x_name] = date.format('MM') + '/' + date.format('YY');
+            new_element[y_name] = d.y;
+            new_data.push(new_element);
+          });
+
+          return resolve(new_data);
+        });
+      });
+  } else if (group_by == "date_year"){
+    return db.Sale.query(function(q){
+      q.column("sales.date as x").sum('value as y')
+        .whereRaw(filter_table_name + '.name = "' + filter_value + '"')
+        .groupByRaw("YEAR(sales.date)")
+        .orderByRaw("YEAR(sales.date)")
+        .innerJoin('departments', 'sales.department_id', 'departments.id')
+        .innerJoin('payment_types', 'sales.payment_type_id', 'payment_types.id');
+
+    }).fetchAll()
+      .then(function(data){
+        return new Promise(function(resolve){
+          var json_data = data.toJSON();
+          var new_data = [];
+          json_data.forEach(function(d){
+            var date = moment(d.x);
+            var new_element = {};
+            new_element[x_name] = date.format('YYYY');
+            new_element[y_name] = d.y;
+            new_data.push(new_element);
+          });
+
+          return resolve(new_data);
+        });
+      });
+  }
+};
+
+
+
 /**
  *
  *
@@ -101,10 +231,10 @@ data_provider.getTreemapDataWithFilter = function(group_by, filter_type, filter_
     var table2 = "provinces";
     //var table3 = "payment_types";
 
-  //} else if (group_by == "store"){
-  //  var table1 = "provinces";
-  //  var table2 = "payment_types";
-  //  var table3 = "departments";
+    //} else if (group_by == "store"){
+    //  var table1 = "provinces";
+    //  var table2 = "payment_types";
+    //  var table3 = "departments";
 
   } else if (group_by == "payment_type"){
     var table1 = "payment_types";
@@ -129,95 +259,6 @@ data_provider.getTreemapDataWithFilter = function(group_by, filter_type, filter_
         return resolve(data.toJSON());
       })
     });
-
-};
-
-
-
-
-/**
- *
- *
- *
- * */
-data_provider.getTreemapData = function(group_by) {
-  if (group_by == "department"){
-    var table1 = "departments";
-    var id1 = "department_id";
-    var table2 = "provinces";
-    var id2 = "province_id";
-    var table3 = "payment_types";
-    var id3 = "payment_type_id";
-
-  } else if (group_by == "store"){
-    var table1 = "provinces";
-    var id1 = "province_id";
-    var table2 = "payment_types";
-    var id2 = "payment_type_id";
-    var table3 = "departments";
-    var id3 = "department_id";
-
-  } else if (group_by == "payment_type"){
-    var table1 = "payment_types";
-    var id1 = "payment_type_id";
-    var table2 = "departments";
-    var id2 = "department_id";
-    var table3 = "provinces";
-    var id3 = "province_id";
-  }
-
-  return db.Sale.query(function(q) {
-    q.column([table1 + ".name as level1", table2 + ".name as level2", table3 + ".name as key"]).sum('value as value')
-      .groupBy(table1 + '.name')
-      .groupBy(table2 + '.name')
-      .groupBy(table3 + '.name')
-      .innerJoin('departments', 'sales.department_id', 'departments.id')
-      .innerJoin('stores', 'sales.store_id', 'stores.id')
-      .innerJoin('provinces', 'stores.province_id', 'provinces.id')
-      .innerJoin('payment_types', 'sales.payment_type_id', 'payment_types.id')
-  }).fetchAll()
-    .then(function(data){
-      return new Promise(function(resolve){
-        //console.log(data.toJSON());
-        return resolve(data.toJSON());
-      })
-    });
-};
-
-
-/**
- *
- *
- *
- * */
-data_provider.getXYDataWithFilter = function(graphic_type, group_by, filter_type, filter_value){
-  if(graphic_type == "line_chart" || graphic_type == "bar_chart"){
-    var x_name = "x";
-    var y_name = "y";
-  } else if(graphic_type == "pie_chart") {
-    var x_name = "key";
-    var y_name = "value";
-  }
-
-  if(group_by == "department" || group_by == "payment_type"){
-    var filter_table_name = filter_type + "s";
-    var table_name = group_by + "s";
-    var id_name = group_by + "_id";
-    return db.Sale.query(function(q){
-
-      q.column(table_name + '.name as ' + x_name).sum('value as ' + y_name)
-        .whereRaw(filter_table_name + '.name = "' + filter_value + '"')
-        .groupBy(id_name)
-        .innerJoin('departments', 'sales.department_id', 'departments.id')
-        .innerJoin('payment_types', 'sales.payment_type_id', 'payment_types.id');
-
-    }).fetchAll()
-      .then(function(data){
-        return new Promise(function(resolve){
-          return resolve(data.toJSON());
-        })
-      });
-  }
 
 };
 
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -140,7 +140,7 @@ $(document).ready(function () {
         text : option.text
       }));
     });
-  }
+  };
 
 
   /* GROUP BY */
